40248a7425298c515a7b708513da208f75f9916f
Hirata Koya <hiratakoya@HiratanoMacBook-Pro.local>
2017-08-13 14:42:40 +0900

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root "pictures#index"
+
+  resources :pictures, only: [:index, :new, :create, :edit, :update, :destroy]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
