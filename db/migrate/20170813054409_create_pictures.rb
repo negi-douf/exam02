0b7fe4f3f5c94be3126e6c1ab25b09991e98e935
Hirata Koya <hiratakoya@HiratanoMacBook-Pro.local>
2017-08-13 14:57:27 +0900

    create pictures_table

--- db/migrate/20170813054409_create_pictures.rb
+++ db/migrate/20170813054409_create_pictures.rb
@@ -1,6 +1,8 @@
 class CreatePictures < ActiveRecord::Migration
   def change
     create_table :pictures do |t|
+      t.string :picture
+      t.string :description
 
       t.timestamps null: false
     end
