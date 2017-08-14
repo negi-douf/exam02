8c9c195dea458a20ad671f53d0be90b0ac16cc93
Hirata Koya <hiratakoya@HiratanoMacBook-Pro.local>
2017-08-14 21:33:09 +0900

    migrate and create views for device

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root "pictures#index"
 
   resources :pictures, only: [:index, :new, :create, :edit, :update, :destroy]
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
