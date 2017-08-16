5ce68790272b21ab3c87392dda7702188c8224cd
Hirata Koya <hiratakoya@HiratanoMacBook-Pro.local>
2017-08-16 23:27:36 +0900

    add authenticate_user! to before_action

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,5 @@
 class PicturesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_picture, only: [:edit, :update, :destroy]
   before_action :set_errors, only: [:new, :edit]
 
