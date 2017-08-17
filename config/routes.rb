30c93422c6f8cc2db644240484a512251160ef66
Hirata Koya <hiratakoya@HiratanoMacBook-Pro.local>
2017-08-17 12:41:12 +0900

    settings letter_opener_web, edit gem

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
 
   resources :pictures, only: [:index, :new, :create, :edit, :update, :destroy]
 
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
