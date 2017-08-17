a8eefcaa4be46e40113d77b16d625de20ea9abc9
Hirata Koya <hiratakoya@HiratanoMacBook-Pro.local>
2017-08-17 22:31:58 +0900

    fixed production.rb, view_registrations/new

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  ActionMailer::Base.delivery_method = { host: "aqueous-earth-25795.herokuapp.com" }
+  
+  config.action_mailer.default_url_options = { host: "aqueous-earth-25795.herokuapp.com" }
+  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
     user_name: ENV['SENDGRID_USERNAME'],
