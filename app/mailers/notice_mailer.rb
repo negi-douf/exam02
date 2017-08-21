0a3ffbc266006ecd02812ce4bed09fc7338b6139
Hirata Koya <hiratakoya@HiratanoMacBook-Pro.local>
2017-08-21 21:28:28 +0900

    fixed mailer

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -1,11 +1,10 @@
 class NoticeMailer < ApplicationMailer
-
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.notice_mailer.sendmail_picture.subject
   #
-  def sendmail_picture(picture)
+  def sendmail_picture
     @greeting = "Hi"
 
     mail to: "atarih.ayuok@gmail.com",
