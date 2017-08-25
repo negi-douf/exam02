47c97540e1bb2b36262ca3ecce48e2a7cf4bb45d
Hirata Koya <hiratakoya@HiratanoMacBook-Pro.local>
2017-08-25 23:03:04 +0900

    to facebook login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@ module ApplicationHelper
   def show_avatar(user)
     return image_tag(user.avatar, alt: user.name, :size => "50x50") unless user.avatar.blank?
     return image_tag(user.image_url, alt: user.name, :size => "50x50") if user.image_url != nil
-    
+
     if ! user.image_url.blank?
       img_url = user.image_url
     elsif ! user.avatar.blank?
