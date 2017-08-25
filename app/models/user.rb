896ece8512015f5ddf0db8608e9228dc67f5c615
Hirata Koya <hiratakoya@HiratanoMacBook-Pro.local>
2017-08-25 23:41:15 +0900

    add dependent to user -> picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
 
-  has_many :pictures
+  has_many :pictures, dependent: :destroy
 
   mount_uploader :avatar, AvatarUploader
 
